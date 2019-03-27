5cae2bfb9ba3e373fac343476a3c38c7d668c0a0
Filippo Cicirelli <pippocicirelli@gmail.com>
2019-03-27 18:08:31 +0100

    Aggiunto supporto MQTT a user  microservizi

---
+++ backend/UserMicroservice/src/main/java/com/UserMicroservice/service/MQTTService.java
@@ -0,0 +1,33 @@
+package com.UserMicroservice.service;
+
+import org.springframework.stereotype.Service;
+
+import com.UserMicroservice.utils.MQTTClient;
+
+@Service
+public class MQTTService {
+	
+	private MQTTClient mqttClient;
+	
+	public boolean connectToMQTTClient(String brokenURL, String topicID, int subQoS) {
+		mqttClient = new MQTTClient(brokenURL, topicID, subQoS);
+		return mqttClient.connect();
+	}
+	
+	public boolean disconnectToMQTTClient() {
+		return mqttClient.disconnect();
+	}
+	
+	public boolean publishMessage(String message) {
+		return mqttClient.pubblish(message);
+	}
+	
+	public boolean subscribeTopic() {
+		return mqttClient.subscribe();
+	}
+	
+	public String getMessage() {
+		return mqttClient.getMessage();
+	}
+
+}
